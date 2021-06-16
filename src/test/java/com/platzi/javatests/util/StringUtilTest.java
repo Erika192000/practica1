7012565524ece2e52d40cff06a3ec0fee13b45b7
Erika Espinoza <erika.espinoza.lopez19@gmail.com>
2021-06-16 16:01:01 -0500

    Creacion de test unitario, lanzar una excepcion para alerta sobre error

--- src/test/java/com/platzi/javatests/util/StringUtilTest.java
+++ src/test/java/com/platzi/javatests/util/StringUtilTest.java
@@ -5,14 +5,15 @@ package com.platzi.javatests.util;
 public class StringUtilTest {
 
     public static void main(String[] args) {
-       String result= StringUtil.repeat("hola", 3);
-        if (!result.equals("holaholahola")){
-            System.out.println("ERROR");
-        }
 
-        String result2= StringUtil.repeat("hola", 1);
-        if(!result2.equals("hola")){
-            System.out.println("ERROR");
+       asserEquals(StringUtil.repeat("hola", 3), "holaholahola" );
+
+        asserEquals(StringUtil.repeat("hola", 1), "hola");
+    }
+
+    private static void asserEquals(String actual, String expected) {
+        if (!actual.equals(expected)){
+            throw new RuntimeException(actual + " is not equal to expected " + expected);
         }
     }
 }
